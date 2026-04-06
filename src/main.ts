a1004c89679ca6793a3faf096f5d90073ae8fe53
RuslanBozorov <frontendruslan@gmail.com>
2026-04-06 18:20:53 +0500

    add_api_ro

--- src/main.ts
+++ src/main.ts
@@ -20,17 +20,15 @@ async function bootstrap() {
   );
 
   
-  if (process.env.NODE_ENV !== 'production') {
-    const config = new DocumentBuilder()
-      .setTitle('Barber Booking API')
-      .setDescription('API documentation for Barbershop Booking System')
-      .setVersion('1.0')
-      .addBearerAuth()
-      .build();
+  const config = new DocumentBuilder()
+    .setTitle('Barber Booking API')
+    .setDescription('API documentation for Barbershop Booking System')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
 
-    const document = SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup('api/docs', app, document);
-  }
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
 
   const port = process.env.PORT || 10000; 
   await app.listen(port);
